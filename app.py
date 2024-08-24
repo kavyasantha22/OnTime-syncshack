2848d2bdd4b63fb6b6db5abc04b924f94a4d9039
Kavya Guna <kavyasantha9@gmail.com>
2024-08-24 17:14:29 +1000

    Working login and register code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import sqlite3
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
@@ -18,9 +18,22 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-OnTimeDB = sqlite3.connect('ontime.db')
-OnTimeCursor = OnTimeDB.cursor()
+# Database setup with g (Flask global context)
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('ontime.db')
+        g.db.row_factory = sqlite3.Row  # To access columns by name
+    return g.db
+
+def get_cursor():
+    db = get_db()
+    return db.cursor()
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 
 @app.after_request
@@ -32,24 +45,24 @@ def after_request(response):
     return response
 
 
+
 @app.route("/")
 @login_required
 def index():
+    print(1)
     """Show portfolio of stocks"""
+    cursor = get_cursor()
+    groups = cursor.execute("SELECT * FROM groups WHERE person_id = ?", (session["user_id"],))
+    schedules = cursor.execute("SELECT * FROM schedules WHERE person_id = ?", (session["user_id"],))
     
-    groups = OnTimeCursor.execute("SELECT * FROM groups WHERE person_id = ?", session["user_id"])
-    schedules = OnTimeCursor.execute("SELECT * FROM schedules WHERE person_id = ?", session["user_id"])
-    
-    # for stock in stocks:
-    #     stock["price"] = lookup(stock["stock_name"])["price"]
-    #     stock["total"] = stock["price"]*stock["shares"]
-    user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]
+    user = cursor.execute("SELECT * FROM users WHERE id = ?", (session["user_id"],)).fetchone()
     return render_template("index.html", groups=groups, schedules=schedules, user = user)
 
 @app.route("/upload-photo", methods=["GET", "POST"])
 @login_required
 def upload_photo():
-    task = OnTimeCursor.execute("""
+    cursor = get_cursor()
+    task = cursor.execute("""
             SELECT * FROM schedules 
             WHERE person_id = ? 
             AND task_status = 0 
@@ -57,12 +70,12 @@ def upload_photo():
             LIMIT 1
         """, (session["user_id"],)).fetchone()
     if request.method == "POST":
-        OnTimeCursor.execute("""
+        cursor.execute("""
             UPDATE schedules
             SET task_status = 1
             WHERE person_id = ? AND task_name = ? AND task_time = ?
         """, (session["user_id"], task["task_name"], task["task_time"]))
-        OnTimeDB.commit()
+        g.db.commit()
         return redirect("/")
     else:
         return render_template("upload_photo.html", task=task)
@@ -75,7 +88,7 @@ def group():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
+    cursor = get_cursor()
     # Forget any user_id
     session.clear()
 
@@ -90,18 +103,20 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
-
+        username = request.form.get("username")
+        rows = cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (username, )
+        ).fetchone()
+        print("#"*30)
+        print("Row Keys:", rows.keys())
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
+        if rows is None or not check_password_hash(
+            rows["hash"], request.form.get("password")
         ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows["id"]
 
         # Redirect user to home page
         return redirect("/")
@@ -126,7 +141,7 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-
+    cursor = get_cursor()
     # Forget any user_id
     session.clear()
 
@@ -149,72 +164,20 @@ def register():
         if password != confirmation:
             return apology("password and confirmed password must be identical", 400)
 
-        check = db.execute("SELECT username FROM users WHERE username = ?", username)
+        check = cursor.execute("SELECT username FROM users WHERE username = ?", (username,)).fetchone()
         print("#"*30, check)
-        if check != []:
+        if check != None:
             return apology("username has been taken", 400)
 
         hash = generate_password_hash(password)
         # Query database for username
-        db.execute(
-            "INSERT INTO users (username, hash) VALUES (?, ?)", username, hash
+        cursor.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)", (username, hash)
         )
+        g.db.commit(    )
         # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
-
-
-
-    """Sell shares of stock"""
-    if request.method == "POST":
-        symbol = request.form["symbol"]
-        stock_info = lookup(symbol)
-        if not stock_info:
-            return apology("invalid stock symbol", 403)
-        user_amount = db.execute(
-            "SELECT shares FROM stock_ownership WHERE person_id = ? and stock_name = ?", session["user_id"], symbol)[0]["shares"]
-
-        try:
-            amount = int(request.form.get("shares"))
-            if amount <= 0:
-                return apology("number of shares must be positive", 400)
-            if amount > user_amount:
-                return apology("number of shares exceed yours", 400)
-        except ValueError:
-            return apology("number of shares must be an integer", 400)
-
-        price = stock_info["price"]
-        total = price * amount
-        print(total)
-
-        # Query the user's current cash
-        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-        print(cash)
-
-        # Check if the user has enough cash to buy
-
-        # Update the user's cash
-        cash += total
-
-        # Update user's stock amount
-        user_amount -= amount
-        db.execute("UPDATE stock_ownership SET shares = ? WHERE person_id = ? and stock_name = ?",
-                   user_amount, session["user_id"], symbol)
-
-        # Insert to activity
-        current_time = datetime.datetime.now(pytz.timezone(
-            "US/Eastern")).strftime('%Y-%m-%d %H:%M:%S')
-        db.execute("INSERT INTO activity (person_id, stock_name, buy_sell, shares_amount, price, date) VALUES (?, ?, ?, ?, ?, ?)",
-                   session["user_id"], symbol, 'sell', amount, price, current_time)
-
-        # Update the user's cash in the users table
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"])
-
-        return redirect("/")
-    else:
-        stocks = db.execute("SELECT * FROM stock_ownership WHERE person_id = ?", session["user_id"])
-        print(stocks)
-        return render_template("sell.html", stocks=stocks)
